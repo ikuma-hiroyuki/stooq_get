bc5af5e0902ac76df150d6991037bcb697d5077d
ikuma-hiroyuki <chemicalbondo@gmail.com>
2021-09-22 10:35:38 +0900

    取得データを事前設定したものと手入力のどちらでも対応できるようにした

--- stooq_get.py
+++ stooq_get.py
@@ -1,21 +1,41 @@
 import os
+import sys
+
 import pandas as pd
+from pandas.errors import EmptyDataError
 
 
-def get_price(code):
-    df = pd.read_csv(f'https://stooq.com/q/d/l/?s={code}&i=d', index_col=0)
-    return df
-
-
-print('Start acquiring stooq data.')
-symbols = ['ES.F', 'NQ.F', 'ZB.F', 'UL.F']
-
-for symbol in symbols:
+def get_csv(code):
     print('Request ' + symbol + ' data.')
-    df = get_price(symbol)
+    df = pd.read_csv(f'https://stooq.com/q/d/l/?s={code}&i=d', index_col=0)
     print('Get ' + symbol + ' data.\n')
     download = os.environ["USERPROFILE"] + r'\Downloads'
-    file = download + '\\' + symbol + '.CSV'
-    df.to_csv(file)
+    filePath = download + '\\' + symbol.replace('.F', '') + '.csv'
+    df.to_csv(filePath)
 
-input('Completed.')
+
+processingMethod = input('Select a processing number .\n'
+                         '1: ES, NQ, ZB, UB(UD).\n'
+                         '2: Manual input.\n')
+
+
+if processingMethod == '1':
+    symbols = ['ES.F', 'NQ.F', 'ZB.F', 'UD.F']
+
+    for symbol in symbols:
+        get_csv(symbol)
+
+elif processingMethod == '2':
+    symbol = input('Please input a symbol.\n')
+    try:
+        get_csv(symbol)
+    except EmptyDataError:
+        input('\n!!ERROR!!\n'
+              'The symbol you entered does not exist.')
+        sys.exit()
+
+else:
+    input('Processing number input error.')
+    sys.exit()
+
+input('Done. Check your downloads folder.')
